f69e744d6df8627ff5779d60af6d1282b4516455
Matheus Silva <19240911+c4lib4n@users.noreply.github.com>
2024-08-08 16:00:27 +0100

    Delete Array.py

--- Array.py
+++
@@ -1,8 +0,0 @@
-playlist = ["Odiosa natureza humana", "Bom e quando faz mal", "Bixo do Mato", "Ouro de tolo"]
-
-for i in range(len(playlist)):
-    print(f"Musica {i +1}: {playlist[i]}")
-playlist.append("Outra musica")
-print("Playlist depois de adicionada a musica: ", playlist)
-playlist.remove("Odiosa natureza humana")
-print("Playlist depois de removida a musica: ", playlist)
