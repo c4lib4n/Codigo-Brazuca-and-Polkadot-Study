d03a6cdc491a2c4f2169c095dbc86a38d372f1e1
Matheus Silva <19240911+c4lib4n@users.noreply.github.com>
2024-08-21 18:37:19 +0100

    Delete Exercises/Python directory

--- Exercises/Python/Exercise 1/main.py
+++
@@ -1,8 +0,0 @@
-#Atividade 1: Imprimindo Mensagens
-#Exemplo: Imprimindo uma saudação
-
-print("Olá, mundo!") # Isto irá imprimir "Olá, mundo!" na tela.
-print("Matheus", " Governador Lindenberg")
-
-#Atividade Prática:
-#Escreva um programa que imprima seu nome e sua cidade natal.
